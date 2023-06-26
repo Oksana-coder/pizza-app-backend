4a9ada5b79ef88e58178c5ebf992eda7eb360e76
Oksana <oksana.palamarchuk@burda-forward>
2023-06-26 15:31:46 +0200

    Changed cors config

--- src/main/java/com/example/pizzaApp/PizzaAppApplication.java
+++ src/main/java/com/example/pizzaApp/PizzaAppApplication.java
@@ -30,10 +30,10 @@ public class PizzaAppApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		User[] users = new User[] {
-			new User("Harry", "harry@gmail.com", "12345", Role.USER),
-			new User("Ron", "ron@gmail.com", "09876", Role.USER),
-			new User("Hermione", "hermione@gmail.com", "qwerty", Role.USER),
-			new User("Neville", "neville@gmail.com", "poiuy", Role.USER),
+			new User("Harry", "harry@gmail.com", bCryptPasswordEncoder().encode("12345"), Role.USER),
+			new User("Ron", "ron@gmail.com", bCryptPasswordEncoder().encode("09876"), Role.USER),
+			new User("Hermione", "hermione@gmail.com", bCryptPasswordEncoder().encode("qwerty"), Role.USER),
+			new User("Neville", "neville@gmail.com", bCryptPasswordEncoder().encode("poiuy"), Role.USER),
 		};
 
 		for (int i = 0; i < users.length; i++) {
@@ -41,10 +41,14 @@ public class PizzaAppApplication implements CommandLineRunner {
 		}
 
 		Pizza[] pizzas = new Pizza[] {
-				new Pizza("Diavolo", 13),
-				new Pizza("Quattro Stagioni", 16),
-				new Pizza("Margherita", 12),
-				new Pizza("Quattro Formaggi", 12),
+				new Pizza("Diavolo", 13, "https://assets.tmecosys.com/image/upload/t_web767x639/img/recipe/ras/Assets/36DFE80A-776A-49F7-B4BB-273C0A702BEE/Derivates/b6c3bc06-581e-4da9-a38f-8b6d47d62891.jpg"),
+				new Pizza("Quattro Stagioni", 16, "https://www.italianstylecooking.net/wp-content/uploads/2022/01/Pizza-quattro-stagioni-1200x900.jpg"),
+				new Pizza("Margherita", 12, "https://cdn.shopify.com/s/files/1/0410/4598/3397/articles/Margherita-9920.jpg?v=1644589966"),
+				new Pizza("Quattro Formaggi", 14, "https://www.tasteatlas.com/images/recipes/946f07b7b5ec4f23a6fea58a2ac72650.jpg?mw=910"),
+				new Pizza("Buffala", 16, "https://www.kuechengoetter.de/uploads/media/630x630/04/42534-pizza-margherita.jpg?v=1-0"),
+				new Pizza("Salami", 12, "https://image.stern.de/5963252/t/5F/v7/w1440/r1/-/salamipizza-jpg--5523cef8c0641645-.jpg"),
+				new Pizza("Prosciutto e Funghi", 12, "https://i1.wp.com/www.piccolericette.net/piccolericette/wp-content/uploads/2019/10/4102_Pizza.jpg?fit=1216%2C616&ssl=1"),
+				new Pizza("Frutti di Mare", 12, "https://la-palma-neuwied.com/pizzaria/wp-content/uploads/2016/08/shutterstock_290954402.jpg"),
 		};
 
 		for (int i = 0; i < pizzas.length; i++) {
@@ -63,7 +67,7 @@ public class PizzaAppApplication implements CommandLineRunner {
 		@Override
 		public void addCorsMappings(CorsRegistry registry) {
 			registry.addMapping("/**")
-					.allowedOrigins("http://127.0.0.1:5173")
+					.allowedOrigins("*")
 					.allowedMethods("*");
 		}
 	};
